a8b3e3e46818bb639751e9cac7bfed4db15b0e24
RicMFM <aluno23306@ipt.pt>
2023-03-16 12:05:34 +0000

    versão inicial

---
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Eq2Grau.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eq2Grau.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
